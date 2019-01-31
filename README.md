c7421624561654d35046cebf49bc9f8d8571f976
Roman Frołow <rofrol@gmail.com>
2019-01-31 15:45:25 +0100

    Add Browser.element

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Install elm, run `elm reactor`.
+
+`Browser.element` example from
+
+- https://guide.elm-lang.org/architecture/
+- https://package.elm-lang.org/packages/elm/browser/latest/Browser#element
+- https://guide.elm-lang.org/effects/http.html
--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,35 @@
 module Main exposing (main)
 
+import Browser
 import Html
 
 
 main =
-    Html.text "Hello"
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    Int
+
+
+init : () -> ( (), Cmd msg )
+init _ =
+    ( (), Cmd.none )
+
+
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions model =
+    Sub.none
+
+
+view model =
+    Html.text
+        "Hello"
