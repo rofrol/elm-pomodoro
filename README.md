412deeda30c1408c780c41bec43deba665c6bc8a
Roman Frołow <rofrol@gmail.com>
2019-01-31 16:34:18 +0100

    Show time

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 Install elm, run `elm reactor`.
 
-`Browser.element` example from
+
+## Browser.element
 
 - https://guide.elm-lang.org/architecture/
 - https://package.elm-lang.org/packages/elm/browser/latest/Browser#element
 - https://guide.elm-lang.org/effects/http.html
+
+## Time
+
+- https://ellie-app.com/3dYXjr3bJNWa1
+- https://stackoverflow.com/questions/29453679/how-do-i-get-the-current-time-in-elm/52156804#52156804
--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html
+import Task
+import Time exposing (Posix)
 
 
 main =
@@ -13,23 +15,76 @@ main =
         }
 
 
+
+-- Model
+
+
 type alias Model =
-    Int
+    { posix : Posix
+    , zone : Time.Zone
+    , running : Bool
+    }
 
 
-init : () -> ( (), Cmd msg )
+
+-- Msg
+
+
+type Msg
+    = Tick Posix
+    | Zone Time.Zone
+    | Running Bool
+
+
+
+-- Init
+
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ( (), Cmd.none )
+    ( Model (Time.millisToPosix 0) Time.utc True, Task.perform Zone Time.here )
+
+
+
+-- Update
 
 
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Tick newPosix ->
+            ( { model | posix = newPosix }, Cmd.none )
+
+        Zone zone ->
+            ( { model | zone = zone }, Cmd.none )
+
+        Running state ->
+            ( { model | running = state }, Cmd.none )
+
+
+
+-- Subscriptions
 
 
 subscriptions model =
-    Sub.none
+    if model.running then
+        Time.every 1000 Tick
+
+    else
+        Sub.none
+
+
+
+-- View
 
 
 view model =
-    Html.text
-        "Hello"
+    Html.text <|
+        formatTime model.zone model.posix
+
+
+formatTime zone posix =
+    (String.padLeft 2 '0' <| String.fromInt <| Time.toHour zone posix)
+        ++ ":"
+        ++ (String.padLeft 2 '0' <| String.fromInt <| Time.toMinute zone posix)
+        ++ ":"
+        ++ (String.padLeft 2 '0' <| String.fromInt <| Time.toSecond zone posix)
